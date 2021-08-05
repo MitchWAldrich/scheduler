d36ff5a52d95ff182a67298e4928b4a9dea5b487
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-05 19:40:06 -0400

    add Appointment directory to house the appointment's components

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+/*
+State: name(string), interviewer(number)
+Actions: setName, setInterviewer
+Props: name(string), interviewers(array), interviewer(num), onSave, onCancel
+*/
+
+export default function Form(props) {
+  const { interviewers, onSave, onCancel } = props;
+
+  const [name, setName] = useState(props.name || "")
+  const [interviewer, setInterviewer] = useState(props.interviewer || null)
+
+  const reset = function() {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = function() {
+    reset();
+    onCancel();
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)} 
+          />
+        </form>
+        <InterviewerList interviewers={interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={() => cancel()}>Cancel</Button>
+          <Button confirm onClick={onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
