f36134eb56c1f85a67bcd4cf81b1f9852acc8cad
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-05 19:55:34 -0400

    add day and appointments functionality to Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
+import "components/Button.scss";
+import "components/DayListItem.scss";
 import "components/Application.scss";
 
+// import Button from "./Button";
+// import DayListItem from "./DayListItem";
+import DayList from "./DayList";
+import Appointment from "./Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Frank Lloyd-Wright",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Randall Grichuk",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
+  const [days, setDays] = useState([]);
+  useEffect(() => {
+    axios.get('/api/days')
+    .then((response) => {
+      console.log(response.data)
+      setDays(response.data)
+    })
+  }, []);
+
+  const [day, setDay] = useState("Monday");
+
+  const parsedAppointments = appointments.map(appointment => 
+    <Appointment 
+      key={appointment.id}
+      time={appointment.time}
+      interview={appointment.interview}
+      {...appointment}
+      />)
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +86,13 @@ export default function Application(props) {
   alt="Interview Scheduler"
 />
 <hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"></nav>
+<nav className="sidebar__menu">
+  <DayList 
+    days={days}
+    day={day}
+    setDay={setDay} />
+
+</nav>
 <img
   className="sidebar__lhl sidebar--centered"
   src="images/lhl.png"
@@ -20,7 +100,8 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {parsedAppointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
     
