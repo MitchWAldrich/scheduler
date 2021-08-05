9a037a6a7f28bcc6bc1fa99b732b1825ff29c4fe
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-05 19:45:59 -0400

    add functionality to InterviewerListItem with shortcircuiting for selected display

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classNames from 'classnames/bind';
+
+
+import "components/InterviewerListItem.scss";
+
+//id:number
+//name:string
+//avatar:url
+//selected:boolean
+//setInterviewer:function
+export default function InterviewerListItem(props) {
+  const { id, name, avatar, selected, setInterviewer } = props;
+  
+  let interviewerClass = classNames('interviewers__item', {
+    'interviewers__item--selected': selected
+  })
+
+  return (
+    <li 
+    className={interviewerClass}
+    onClick={setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {selected && name}
+    </li>
+  )
+}
