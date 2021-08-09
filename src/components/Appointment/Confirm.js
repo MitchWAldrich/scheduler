0e3c199b0cb609232afd62352eff0229825246a1
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-09 15:51:51 -0400

    update onConfirm functionality

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -3,14 +3,14 @@ import React from "react";
 import Button from "components/Button";
 
 export default function Confirm(props) {
-  const { message, onConfirm, onCancel } = props;
+  const { message, onCancel } = props;
 
   return (
     <main className="appointment__card appointment__card--confirm">
       <h1 className="text--semi-bold">{message}</h1>
       <section className="appointment__actions">
         <Button danger onClick={onCancel}>Cancel</Button>
-        <Button danger onClick={onConfirm}>Confirm</Button>
+        <Button danger onClick={props.onDelete}>Confirm</Button>
       </section>
     </main>
   )
