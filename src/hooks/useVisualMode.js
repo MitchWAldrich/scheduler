7f16e87d00c3336fef4d1b3b544a758667f7c3a2
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-09 15:30:12 -0400

    create useVisualMode to transiton between Appointment modes

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,17 +4,24 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
-    history.push(newMode)
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      history.pop();
+    }
+    history.push(newMode);
     setHistory(history);
     setMode(history[history.length-1]);
-    return mode
+    return mode;
   }
   const back = () => {
-    history.pop()
-    setHistory(history)
-    setMode(history[history.length-1])
-    return mode
+    if (history.length > 1) {
+    history.pop();
+    setHistory(history);
+    setMode(history[history.length-1]);
+    return mode;
+    } else {
+      return mode;
+    }
   }
   
 
