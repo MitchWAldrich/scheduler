157ff254e47745920029a72421aca6142f9b003b
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-09 15:31:00 -0400

    debug getInterview function to render proper student name

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -17,7 +17,7 @@ export function getAppointmentsForDay(state, day) {
       }
     }
   }
-
+  
   return appointments;
 }
 
@@ -25,20 +25,77 @@ export function getInterview(state, interview) {
   if (interview === null) {
     return null;
   }
-  const interviewerID = interview.interviewer;
+  const interviewerID = interview.interviewer
   
   for (let appt in state.appointments) {
-    
-    for (let interviewer in state.interviewers) {
+    let interviewObj = state.appointments[appt].interview
+    let interviewerObj = {};
 
-      for (let interview in state.appointments[appt].interview) {
-        if (interviewerID === state.appointments[appt].interview[interview]) {
-          let interviewObj = state.appointments[appt].interview;
-          interviewObj = { ...interviewObj, interviewer: state.interviewers[interviewer] }
-          return interviewObj;
+    if (!state.appointments[appt].interview) {
+      continue
+    } else if (state.appointments[appt].interview.interviewer === interviewerID && interview.student === state.appointments[appt].interview.student) {
+      
+      for (let interviewer in state.interviewers) {
+        if (state.interviewers[interviewer].id === interviewerID) {
+          interviewerObj = state.interviewers[interviewer]
         }
+      }
+      
+      interviewObj = {...interviewObj, interviewer: interviewerObj}
+      return interviewObj
+    }
+  }
+}
+
+// export function getInterview(state, interview) {
+//   if (interview === null) {
+//     return null;
+//   }
+//   const interviewerID = interview.interviewer;
+  
+//   for (let appt in state.appointments) {
+    
+    
+
+//       for (let interview in state.appointments[appt].interview) {
+//         if (interviewerID === state.appointments[appt].interview[interview]) {
+//           let interviewObj = state.appointments[appt].interview;
+       
+//           let interviewerName = state.appointments[appt].interview.interviewer;
+
+//           for (let name in state.interviewers) {
+//             if (state.interviewers[name].id === interviewerName) {
+//               interviewerName = state.interviewers[name];
+//             }
+//           }
+//           interviewObj = { ...interviewObj, interviewer: interviewerName }
+         
+//           return interviewObj;
+//         }
+//       }
+//     }
+  
+// }
+
+export function getInterviewersForDay(state, day) {
+  const interviewers = [];
+  const filteredDay = state.days.filter(item => item.name === day);
+
+  if (state.days.length === 0 || filteredDay.length === 0) {
+    return [];
+  }
+ 
+  const daysInterviewers = filteredDay[0].interviewers;
+  for (let elem of daysInterviewers) {
+
+    for (let item in state.interviewers) {
+     
+      if (elem === state.interviewers[item].id) {
+        interviewers.push(state.interviewers[item])
       }
     }
   }
+
+  return interviewers;
 }
 
