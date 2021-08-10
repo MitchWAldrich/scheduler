b11dee5a81af9b8ad01ac26e859ca82a4cdfebf3
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-09 23:07:09 -0400

    debug function to properly replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,25 +5,21 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
   const transition = (newMode, replace = false) => {
+    setMode(newMode);
     if (replace) {
-      history.pop();
+    setHistory(prev => ([...prev]));
+    } else {
+    setHistory(prev => ([...prev, newMode]));
     }
-    history.push(newMode);
-    setHistory(history);
-    setMode(history[history.length-1]);
-    return mode;
   }
+
   const back = () => {
     if (history.length > 1) {
     history.pop();
     setHistory(history);
     setMode(history[history.length-1]);
-    return mode;
-    } else {
-      return mode;
-    }
+    } 
   }
-  
 
   return { mode, transition, back };
 }
