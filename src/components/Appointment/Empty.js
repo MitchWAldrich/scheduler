95ebfc69fec003be7e1d758aaeefd3ae882c0c8f
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-12 00:12:49 -0400

    format code and update props names

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,16 +1,15 @@
 import React from "react";
 
 export default function Empty(props) {
-  const { onAdd } = props;
 
   return (
     <main className="appointment__add">
-  <img
-    onClick={onAdd}
-    className="appointment__add-button"
-    src="images/add.png"
-    alt="Add"
-  />
-</main>
+      <img
+        onClick={props.onAdd}
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+      />
+    </main>
   )
 }
