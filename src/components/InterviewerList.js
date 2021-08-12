a007db83e709ea6440fe6e7cdfb17a0bb1589e29
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-11 23:44:55 -0400

    unchanged code

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,25 +1,26 @@
 import React from "react";
+import PropTypes from 'prop-types';
 // import classNames from 'classnames/bind';
-
 
 import "components/InterviewerList.scss";
 
 import InterviewerListItem from "./InterviewerListItem";
+
 
 export default function InterviewerList(props) {
   const { interviewers, interviewer, setInterviewer } = props;
   
   const parsedInterviewers = interviewers.map(data => 
     <InterviewerListItem
-      key={data.id}
-      name={data.name}
-      avatar={data.avatar}
-      selected={data.id === interviewer}
-      setInterviewer={(event) => setInterviewer(data.id)} 
-      />); //don't need to {...data} ??
-
-  return (
-    <section className="interviewers">
+    key={data.id}
+    name={data.name}
+    avatar={data.avatar}
+    selected={data.id === interviewer}
+    setInterviewer={(event) => setInterviewer(data.id)} 
+    />); //don't need to {...data} ??
+    
+    return (
+      <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">
         {parsedInterviewers}
@@ -27,3 +28,7 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
