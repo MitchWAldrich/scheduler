802ccd804dd6a3c6fdf3e0caf90d29009bab8ce2
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-12 00:42:46 -0400

    add comments

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -5,6 +5,7 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   const { days, day, setDay } = props;
   
+  //map through days to render a day object
   const parsedDays = days.map(data => 
     <DayListItem
       key={data.id}
