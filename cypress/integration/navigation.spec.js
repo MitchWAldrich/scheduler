0da944a421e592b95547551d819deeceebe1917e
MitchWAldrich <mitch.w.aldrich@gmail.com>
2021-08-11 23:57:21 -0400

    adjust spacing

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,6 +3,7 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+  
   it("should navigate to Tuesday", () => {
     cy.visit("/");
     cy.contains("[data-testid=day]", "Tuesday")
@@ -11,29 +12,3 @@ describe("Navigation", () => {
   });
 });
 
-// should book an interview
-/* 
-visit the root
-click on "add" in second appointment
-enter name
-Choose interviewer
-click save
-sees booked appt
- */
-
-// should edit an interview
-/*
-Visit root
-Click edit for existing
-change name and interviewer
-click save
-see the edit
-*/
-
-// should cancel an interview
-/*
-visit root
-click delete on existing
-click confirm
-see empty appt slot
-*/
